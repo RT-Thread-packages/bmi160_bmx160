d1e4a4629b4a34303e3eb7d4b19188c7b00ee8d5
286643536 <286643536@qq.com>
2024-10-08 14:55:53 +0800

    [rsoc][sensor_v1]bmi160_bmx160 pkg adapt sensor_v1 frame

--- sensor_bosch_bmx160.h
+++ sensor_bosch_bmx160.h
@@ -1,7 +1,14 @@
 #ifndef SENSOR_BOS_BMI160_H__
 #define SENSOR_BOS_BMI160_H__
 
-#include <sensor.h>
+#include <rtthread.h>
+#if defined(RT_VERSION_CHECK)
+    #if (RTTHREAD_VERSION >= RT_VERSION_CHECK(5, 1, 0))
+        #include "drivers/sensor.h"
+    #else
+        #include "sensor.h"
+    #endif
+#endif
 
 int rt_hw_bmx160_init(const char *name, struct rt_sensor_config *cfg);
 
